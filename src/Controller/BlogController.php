0e92e9b765d9dfd5f14cdf8f594b7577e064b1d9
Ghaffaru <mudashiruagm@gmail.com>
2020-09-03 16:13:47 +0000

    server port fix

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,17 +1,41 @@
 <?php
 namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-class BlogController
+class BlogController extends AbstractController
 {
+    public const POSTS = [
+      [
+          "id" => 1,
+          "title" => "Title 1",
+          "body" => "Body 1"
+      ],
+      [
+          "id" => 2,
+          "title" => "Title 2",
+          "body" => "Body 3"
+      ]
+    ];
     /**
      * @Route("/", name="home")
      */
     public function index()
     {
+        return $this->json([
+            'posts' => self::POSTS
+        ], 200);
+    }
+
+    /**
+     * @Route("/{id}", name="show", requirements={"id"="\d+"})
+     */
+    public function show($id)
+    {
         return new JsonResponse([
-            'message' => 'Blog API is running'
+            "post" => self::POSTS[array_search($id, array_column(self::POSTS, 'id'))]
         ]);
     }
 }
